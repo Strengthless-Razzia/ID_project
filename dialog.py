32536e9027ca68300a6c28fabc486e75f8b353c6
Strengthless-Razzia <58416668+Strengthless-Razzia@users.noreply.github.com>
2022-02-08 13:33:02 +0100

    updates (oups j'avais oublié)

--- dialog.py
+++ dialog.py
@@ -1,40 +1,42 @@
-'''
-Reception des données émise par un Arduino branché
-à un port USB.
-'''
-
 import serial
 import serial.tools.list_ports
+import re
 
-print("Recherche d'un port serie...")
 
-ports = serial.tools.list_ports.comports(include_links=False)
+def recupDonne():
+    print("Recherche d'un port serie...")
 
-if (len(ports) != 0): # on a trouvé au moins un port actif
+    ports = serial.tools.list_ports.comports(include_links=False)
 
-    if (len(ports) > 1):     # affichage du nombre de ports trouvés
-        print (str(len(ports)) + " ports actifs ont ete trouves:") 
-    else:
-        print ("1 port actif a ete trouve:")
+    if (len(ports) != 0): # on a trouvé au moins un port actif
 
-    ligne = 1
+        if (len(ports) > 1):     # affichage du nombre de ports trouvés
+            print (str(len(ports)) + " ports actifs ont ete trouves:") 
+            ligne = 1
+            for port in ports :  # affichage du nom de chaque port
+                print(str(ligne) + ' : ' + port.device)
+                ligne = ligne + 1
+            portChoisi = int(input('Ecrivez le numero precedant le port desire:'))
+        else:
+            print ("1 port actif a ete trouve et selectionne")
+            portChoisi = 1
+            
+        
+        baud = 9600
+        # on établit la communication série
+        arduino = serial.Serial(ports[portChoisi - 1].device, baud, timeout=1)
 
-    for port in ports :  # affichage du nom de chaque port
-        print(str(ligne) + ' : ' + port.device)
-        ligne = ligne + 1
-    portChoisi = int(input('Ecrivez le numero du port desire:'))
-    
-    baud = 9600
-    # on établit la communication série
-    arduino = serial.Serial(ports[portChoisi - 1].device, baud, timeout=1)
+        print('Connexion a ' + arduino.name + ' a un baud rate de ' + str(baud))
 
-    print('Connexion a ' + arduino.name + ' a un baud rate de ' + str(baud))
+        # si on reçoit un message, on l'affiche
+        while True:
+            reco = "[0-9]+$"
+            data = arduino.readline()[:-2]
+            if data:
+                prog = re.compile(reco)
+                result = prog.search(data)
+                
 
-    # si on reçoit un message, on l'affiche
-    while True:
-        data = arduino.readline()[:-2]
-        if data:
-            print(data)
-
-else: # on n'a pas trouvé de port actif
-    print("Aucun port actif n'a ete trouve")
+    else: # on n'a pas trouvé de port actif
+        print("Aucun port actif n'a ete trouve")
+        return(temperature, humidite, occupation, occupation_max)
